a0ff9e7c1f8faa09805bfb1adb91f29181cf5022
oceanp8 <148895610+oceanp8@users.noreply.github.com>
2025-04-30 19:27:37 +0000

    finish

--- GridPath.java
+++ GridPath.java
@@ -1,27 +1,50 @@
-public class GridPath {
+
+import java.net.InterfaceAddress;
+
+public class GridPath 
+{
+
     private int[][] grid;
 
-    public GridPath(int[][] values){
-    }
-    public Location getNextLoc(int row, int col) {
-    int below = grid[row+1][col];
-    int right = grid[row][col+1];
-    if (below>right) return new Location(row, col+1);
-    else return new Location(row+1,col);
+    public GridPath(int[][] values)
+    {
+        grid = values;
     }
 
-    public int sumPath(int row, int col) {
-        return 0;    
-    }
-
-    public String toString(){
-        String S = "";
-        for (int[] row:grid){
-            for (int col:row){
-                S+=col+"";
+    public String toString()
+    {
+        String s = "";
+        for(int row = 0; row < grid.length; row++)
+        {
+            for (int col = 0; col < grid[0].length; col++)
+            {
+                s += grid[row][col] + " ";
             }
-            S+="\n";
+            s += "\n";
         }
-        return S;
+        return s;
+    }
+
+    public Location getNextLoc(int row, int col) 
+    {
+        int below = Integer.MAX_VALUE;
+        if (row < grid.length - 1) below = grid[row+1][col];
+        int right = Integer.MAX_VALUE;
+        if (col < grid[0].length - 1) right = grid[row][col+1];
+        if(below<right) return new Location(row+1, col);
+        else return new Location(row, col+1);
+    }
+
+    public int sumPath(int row, int col) 
+    {
+        int sum = grid[row][col];
+        while(row < grid.length - 1 || col < grid[0].length - 1)
+        {
+            Location loc = getNextLoc(row, col);
+            row = loc.getRow();
+            col = loc.getCol();
+            sum += grid[row][col];
+        }
+        return sum;
     }
 }
